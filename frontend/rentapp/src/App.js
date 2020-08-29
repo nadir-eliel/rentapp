47c93302c3475afd0f46ab08b9508085c65be0bb
emamosegue <emanuelmosegue@gmail.com>
2020-08-29 20:55:13 -0300

    Add files via upload

--- frontend/rentapp/src/App.js
+++ frontend/rentapp/src/App.js
@@ -4,18 +4,33 @@ import './App.css';
 import Commentaries from './components/Users';
 import Buildings from './components/Buildings';
 import Users from './components/Buildings';
+import Navbar from './components/Navbar';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  menuButton,
+} from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      vistaActual: null,
-    };
-  }
-
-  render() {
-    return <div className="App">"Homepage"</div>;
-  }
+function App() {
+  return <Navbar />;
+  /*
+  return (
+    <Router>
+      <div className="container">
+        <h1>Todo bien</h1>
+        <hr />
+        <Switch>
+          <Route path="/login">Pagina de logueo</Route>
+          <Route path="/signin">Pagina de registro</Route>
+          <Route path="/">Pagina de inicio</Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+  */
 }
 
 export default App;
