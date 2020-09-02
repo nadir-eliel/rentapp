19c28cc7e89df40ae0fd21ed2c89314c48d17d2c
emamosegue <emanuelmosegue@gmail.com>
2020-09-02 20:23:24 -0300

    Add files via upload

---
+++ frontend/rentapp/components/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userSelected: undefined,
+    };
+  }
+
+  render() {
+    return <div className="Users">"User"</div>;
+  }
+}
