f02bf2480885564bd2defe198fe1a6b5a34ebce0
pato <patohrg@gmail.com>
2020-09-06 20:51:55 -0300

    agregado lo de cargar imagenes y acomodando modelos

--- backend/config.js
+++ backend/config.js
@@ -1,17 +1,8 @@
-//config.js
-/*module.exports = {
-    PORT: process.env.PORT || 3000,
-    HOST: process.env.APP_HOST,
-    DB: process.env.MONGODB_URI || 'mongodb+srv://patricio:grupo5@cluster0.ibf0e.mongodb.net/rentappDB?retryWrites=true&w=majority',
-  }
-  */
-  const config = {
+const config = {
       //host: process.env.APP_HOST,
       host: 'http://localhost', //Funciona
-      //PORT: process.env.PORT || process.env.APP_PORT,
       port: process.env.PORT || 3000,
       MONGODB: process.env.MONGODB_URI || 'mongodb+srv://patricio:grupo5@cluster0.ibf0e.mongodb.net/rentappDB?retryWrites=true&w=majority',
     }
-
 
   module.exports = config;
--- backend/models/Apartament.js
+++ backend/models/Apartament.js
@@ -8,35 +8,100 @@ const Schema = mongoose.Schema;
 
 // Creamos el objeto del esquema y sus atributos
 const ApartamentSchema = Schema({
-    price: {type: Number, required: true},
-    bathroom_count: {type: Number, required: true},
-    bedroom_count: {type: Number, required: true},
-    room_count: {type: Number, required: true},
-    other_details:{type: String, uppercase: true}, 
-    garage: {type: Boolean, required: true},
-    kids: {type: Boolean, required: true},
-    pets: {type: Boolean, required: true},
-    backyard: {type: Boolean, required: true},
-    pool: {type: Boolean, required: true},
-    //address:{line:{type: String, uppercase: true, required: true}, city: {type: String, uppercase: true, required:true}, province: {type: String, uppercase: true, required: true}, link_map:{type: String}},
-    address:{line:{type: String, uppercase: true}, city: {type: String, uppercase: true}, province: {type: String, uppercase: true}, link_map:{type: String}},
-    //photo:{type:String, required: true},
-    //photo:{type:String},
+    price: {
+        type: Number, 
+        required: true
+    },
+    bathroom_count: {
+        type: Number, 
+        required: true
+    },
+    bedroom_count: {
+        type: Number, 
+        required: true
+    },
+    room_count: {
+        type: Number, 
+        required: true
+    },
+    other_details:{
+        type: String, 
+        uppercase: true
+    }, 
+    garage: {
+        type: Boolean, 
+        required: true
+    },
+    kids: {
+        type: Boolean, 
+        required: true
+    },
+    pets: {
+        type: Boolean, 
+        required: true
+    },
+    backyard: {
+        type: Boolean, 
+        required: true
+    },
+    pool: {
+        type: Boolean, 
+        required: true
+    },
+    address:{
+        line:{
+            type: String, 
+            uppercase: true, 
+            required: true
+        }, 
+        city: {
+            type: String, 
+            uppercase: true, 
+            required:true
+        }, 
+        province: {
+            type: String, 
+            uppercase: true, 
+            required: true
+        }, 
+        link_map:{
+            type: String
+        },
+    },
     photos: [String],
-    rates: [{user_name:String , rate:Number, rate_date:Date}],
-    score_date: {type: Number},
-    comments_count:{type: Number},
-    created_at: {type : Date, default: Date.now },
-    update_at: {type : Date, default: Date.now },
-    user : {type: Schema.Types.ObjectId, ref: 'User' },
+    rates: [{
+        user_name:String, 
+        rate:Number, 
+        rate_date:Date
+    }],
+    score_date: {
+        type: Number
+    },
+    comments_count:{
+        type: Number
+    },
+    created_at: {
+        type : Date, 
+        default: Date.now 
+    },
+    update_at: {
+        type : Date, 
+        default: Date.now 
+    },
+    user : {
+        type: Schema.Types.ObjectId, 
+        ref: 'User' 
+    },
 });
 
+//Creamos una metodo setImgUrl para almecenar las imagenes
 ApartamentSchema.methods.setImgUrl = function setImgUrl(filename) {
    const port = config.port;
    const host = config.host;
    //this.photo = `${host}:${port}/public/${filename}`;
    this.photos.push(`${host}:${port}/public/${filename}`);
 }
+
 
 // Exportamos el modelo para usarlo en otros ficheros
 module.exports = mongoose.model('Apartament', ApartamentSchema);
